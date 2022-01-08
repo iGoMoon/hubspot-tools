1d224d26a2949c9375802d943848e3e1e90be8da
Frank Fava <frank.fava@igomoon.com>
2022-01-09 02:22:24 +1100

    Setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	bracketSpacing: true,
+	printWidth: 80,
+	proseWrap: 'never',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: 'es5',
+	useTabs: false,
+	arrowParens: 'avoid',
+};
